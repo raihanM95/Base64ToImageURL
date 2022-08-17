c5e5c42ec84fc83b2f3c77138d5b64fa3f02a977
raihanM95 <mahmud.raihan@yahoo.com>
2022-08-17 11:58:34 +0600

    upload file

---
+++ Controllers/UploadImageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Base64ToImageURL.Models;
+using System.IO;
+using System;
+
+namespace Base64ToImageURL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadImageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(UploadImage uploadImage)
+        {
+            byte[] bytes = Convert.FromBase64String(uploadImage.Image);
+
+            Image image;
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                image = Image.FromStream(ms);
+            }
+
+            image.Save("wwwroot/images/test2." + uploadImage.Type, ImageFormat.Png);
+
+            return Ok();
+        }
+    }
+}
---
+++ Models/UploadImage.cs
@@ -0,0 +1,8 @@
+﻿namespace Base64ToImageURL.Models
+{
+    public class UploadImage
+    {
+        public string Image { get; set; }
+        public string Type { get; set; }
+    }
+}
